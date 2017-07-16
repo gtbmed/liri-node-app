350cf8563a106b1c13f80f4f17e494a9fb845523
gtbmed <jkbarson@gmail.com>
2017-07-15 21:05:10 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+};
+
+function cat() {
+  console.log('wat is going on');
+};
