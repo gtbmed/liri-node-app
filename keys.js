dce5b59d7c56c72f8aa1fe9c2188c2f98d131874
gtbmed <jkbarson@gmail.com>
2017-07-16 17:22:43 -0400

    added initial setup for assignments

--- keys.js
+++ keys.js
@@ -1,12 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-};
-
-function cat() {
-  console.log('wat is going on');
+  consumer_key: 'HwXNTUX4us7wWy3CUuo6jchJj',
+  consumer_secret: 'htWrxqbsAvnGUyo66uwDzMIzSyKOF2bRcQDKJu7BLjDo63E49j',
+  access_token_key: '886398206443126784-7rE0IhuObSXz7Z1i5n1YPfIhijMOxAD',
+  access_token_secret: 'HjesOotiHBupjvI8rMxQJtiqTs1135t8OMJ2fhaOg4gYO',
 };
---
+++ liri.js
@@ -0,0 +1,4 @@
+var twitterKeys = require('./keys.js');
+var twitterPackage = require('twitter');
+var spotifyAPI = require('node-spotify-api');
+var request = require('request');
