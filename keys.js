f7f7cd7c4ccf11eae6bcf5635ccc1762acd506d9
gtbmed <jkbarson@gmail.com>
2017-07-16 21:48:48 -0400

    abandonded object approach for switch case, added Spotify keys, added JSON dependencies and keywords

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '886398206443126784-7rE0IhuObSXz7Z1i5n1YPfIhijMOxAD',
   access_token_secret: 'HjesOotiHBupjvI8rMxQJtiqTs1135t8OMJ2fhaOg4gYO',
 };
+
+exports.spotifyKeys = {
+  client_id: '016f2ffd735f4fe99e048e70d99d447c',
+  client_secret: '6b3156db04e843f886c550d2c0583d1b',
+};
--- liri.js
+++ liri.js
@@ -1,4 +1,53 @@
 var twitterKeys = require('./keys.js');
 var twitterPackage = require('twitter');
 var spotifyAPI = require('node-spotify-api');
+var spotifyKeys = require('./keys.js');
 var request = require('request');
+var fs = require('file-system');
+
+// Input Arguments
+var userCommand = process.argv[2];
+var userInput = process.argv[3];
+
+
+//Switch Case
+
+switch (userInput) {
+        case 'my-tweets':
+            myTweets();
+            break;
+
+        case 'spotify-this-song':
+
+        break;
+
+        case 'movie-this':
+
+        break;
+
+        case 'do-what-it-says': 
+
+        break;
+
+        default:
+        
+
+};
+
+function myTweets () {
+        request("https://api.twitter.com/1.1/statuses/user_timeline.json?@boot_hw=twitterapi&count=20", function(error, tweets, response) {
+            console.log(tweets);
+            console.log("this works");
+        })
+    }
+
+// // Movie - This
+// request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+//   // If there were no errors and the response code was 200 (i.e. the request was successful)...
+//   if (!error && response.statusCode === 200) {
+
+//     // Then we print out the imdbRating
+//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+//   }
+// });
