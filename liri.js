1ebcb29f4c7fe5971d7aed5d775e005ca1cacee2
gtbmed <jkbarson@gmail.com>
2017-07-16 22:40:03 -0400

    Finally got the twitter call working, next step is parsing

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-var twitterKeys = require('./keys.js');
+var tKeys = require('./keys.js');
 var twitterPackage = require('twitter');
 var spotifyAPI = require('node-spotify-api');
 var spotifyKeys = require('./keys.js');
@@ -12,10 +12,10 @@ var userInput = process.argv[3];
 
 //Switch Case
 
-switch (userInput) {
+switch (userCommand) {
         case 'my-tweets':
             myTweets();
-            break;
+        break;
 
         case 'spotify-this-song':
 
@@ -34,13 +34,25 @@ switch (userInput) {
 
 };
 
-function myTweets () {
-        request("https://api.twitter.com/1.1/statuses/user_timeline.json?@boot_hw=twitterapi&count=20", function(error, tweets, response) {
-            console.log(tweets);
-            console.log("this works");
-        })
-    }
+function myTweets() {
+    var client = new twitterPackage ({
+        consumer_key: tKeys.twitterKeys.consumer_key,
+        consumer_secret: tKeys.twitterKeys.consumer_secret,
+        access_token_key: tKeys.twitterKeys.access_token_key,
+        access_token_secret: tKeys.twitterKeys.access_token_secret
+    });
 
+    //Create our parameters for the Twitter API
+    var parameters = {screen_name: 'boot_hw', count: '20'};
+
+    client.get('statuses/user_timeline', parameters, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        } else {
+            console.log(error);
+        }
+    });
+}
 // // Movie - This
 // request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
