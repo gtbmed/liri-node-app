837b50f782d1629ec1d53ccf8fd865c16dfaaf93
gtbmed <jkbarson@gmail.com>
2017-07-17 22:55:30 -0400

    finally got Spotify working

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 var tKeys = require('./keys.js');
 var twitterPackage = require('twitter');
 var spotifyAPI = require('node-spotify-api');
-var spotifyKeys = require('./keys.js');
+var sKeys = require('./keys.js');
 var request = require('request');
 var fs = require('file-system');
 
@@ -10,7 +10,7 @@ var userCommand = process.argv[2];
 var userInput = process.argv[3];
 
 
-//Switch Case
+//Switch Case to switch between functions based upon user input
 
 switch (userCommand) {
         case 'my-tweets':
@@ -18,11 +18,13 @@ switch (userCommand) {
         break;
 
         case 'spotify-this-song':
-
+            var songName = userInput;            
+            spotifyThis(songName);
         break;
 
         case 'movie-this':
-
+            movieName = userInput;
+            movieThis(movieName);
         break;
 
         case 'do-what-it-says': 
@@ -34,6 +36,8 @@ switch (userCommand) {
 
 };
 
+// Functions for the switch case
+// 1. myTweets gets the most recent 20 tweets from my test Twitter account
 function myTweets() {
     var client = new twitterPackage ({
         consumer_key: tKeys.twitterKeys.consumer_key,
@@ -43,17 +47,55 @@ function myTweets() {
     });
 
     //Create our parameters for the Twitter API
-    var parameters = {screen_name: 'boot_hw', count: '20'};
+    var parameters = {screen_name: 'boot_hw', count: '20'}; // Limits which user we get the tweets from and how many
 
     client.get('statuses/user_timeline', parameters, function(error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            for (var i = 0; i < tweets.length; i++) { // Runs through the tweets array and logs each of the tweets at their specific time
+                var tweet = tweets[i].text;
+                var tweetTime = tweets[i].created_at;
+                console.log('boot_hw tweeted "' + tweet + '" at ' + tweetTime);
+            }
         } else {
-            console.log(error);
+            console.log("Error: "+ error);
         }
     });
 }
-// // Movie - This
+
+//2. spotifyThis 
+function spotifyThis(songName) {
+    // Authenticate the connection to Spotify API
+    var spotify = new spotifyAPI ({
+        id: sKeys.spotifyKeys.client_id,
+        secret: sKeys.spotifyKeys.client_secret
+    });
+    // If the user leaves the song name input blank, we'll spam them with Ace of Base
+    if (songName == null) {
+        songName = 'The Sign Ace of Base';
+    }
+    // Have a separate parameters variable for the songName in case we need to set it for the null case scenario
+    var parameters = songName;
+    // Create the search of the spotify and input our parameters
+    spotify.search({ type: 'track', query: parameters }, function(error, data) {
+        if (!error && songName != null) {
+          for (var i = 0; i < data.tracks.items.length; i++) {
+            var artists = data.tracks.items[i].artists[0].name; //This took forever to find in the API documentation
+            var name = data.tracks.items[i].name;
+            var preview = data.tracks.items[i].preview_url;
+            var album = data.tracks.items[i].album.name;
+            console.log('=================================');
+            console.log('Artists: ' + artists);
+            console.log('Song Name: '+ name);
+            console.log('Preview URL: '+ preview);
+            console.log('Album Name: '+ album);
+            console.log('=================================');
+          }  
+        } else {
+            console.log("Error: "+ error)
+         }
+    })
+}
+    // // Movie - This
 // request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
 //   // If there were no errors and the response code was 200 (i.e. the request was successful)...
